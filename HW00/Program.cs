04eea5ad1a206f0fca75c7988a01d974e1487c22
TomasHutnan <tomas.hutnan147@gmail.com>
2026-02-22 13:15:55 +0100

    feat: better encapsulation and validations in Course

--- HW00/Program.cs
+++ HW00/Program.cs
@@ -11,14 +11,14 @@ PB178.Lecture.AddWeeklySession(new DateTime(2026, 2, 19, 14, 0, 0), new DateTime
 
 Seminar seminar = new("PB178/10", false, ["T. Cvejn", "E. Hatalčíková"]);
 seminar.AddWeeklySession(new DateTime(2026, 2, 17, 10, 0, 0), new DateTime(2026, 2, 17, 11, 50, 0), "C119", 13);
-PB178.Seminars.Add("10", seminar);
+PB178.AddSeminar(seminar);
 
 seminar = new Seminar("PB178/07", false, ["K. Švadlenková", "M. Tvarožek"]);
 seminar.AddWeeklySession(new DateTime(2026, 2, 18, 12, 0, 0), new DateTime(2026, 2, 18, 13, 50, 0), "C119", 13);
-PB178.Seminars.Add("07", seminar);
+PB178.AddSeminar(seminar);
 
 timetable.EnrollCourse(PB178);
-timetable.EnrollSeminarGroup("PB178", "10");
+timetable.EnrollSeminarGroup("PB178", "PB178/10");
 
 Course IV003 = new("Algorithms II", "IV003")
 {
@@ -28,20 +28,20 @@ IV003.Lecture.AddWeeklySession(new DateTime(2026, 2, 18, 14, 0, 0), new DateTime
 
 seminar = new Seminar("IV003/A", false, ["N. Beneš"]);
 seminar.AddWeeklySession(new DateTime(2026, 2, 19, 16, 0, 0), new DateTime(2026, 2, 19, 17, 50, 0), "A218", 13);
-IV003.Seminars.Add("A", seminar);
+IV003.AddSeminar(seminar);
 
 seminar = new Seminar("IV003/02_CZ", false, ["M. Jonáš"]);
 seminar.AddWeeklySession(new DateTime(2026, 2, 19, 8, 0, 0), new DateTime(2026, 2, 19, 9, 50, 0), "A218", 13);
-IV003.Seminars.Add("02_CZ", seminar);
+IV003.AddSeminar(seminar);
 
 seminar = new Seminar("IV003/03_EN", false, ["M. Jonáš"]);
 seminar.AddWeeklySession(new DateTime(2026, 2, 20, 10, 0, 0), new DateTime(2026, 2, 20, 11, 50, 0), "A218", 13);
-IV003.Seminars.Add("03_EN", seminar);
+IV003.AddSeminar(seminar);
 
 seminar = new Seminar("IV003/2EN_lecture", false, ["I. Černá"]);
 seminar.AddWeeklySession(new DateTime(2026, 2, 17, 16, 0, 0), new DateTime(2026, 2, 17, 17, 50, 0), "A218", 13);
-IV003.Seminars.Add("2EN_lecture", seminar);
+IV003.AddSeminar(seminar);
 
 timetable.EnrollCourse(IV003);
-timetable.EnrollSeminarGroup("IV003", "02_CZ");
-timetable.EnrollSeminarGroup("IV003", "2EN_lecture");
+timetable.EnrollSeminarGroup("IV003", "IV003/02_CZ");
+timetable.EnrollSeminarGroup("IV003", "IV003/2EN_lecture");
--- HW00/timetable/course.cs
+++ HW00/timetable/course.cs
@@ -1,10 +1,81 @@
+using System.Collections.ObjectModel;
+
 namespace pb178.timetable
 {
-    public class Course(string title, string code)
+    public class Course
     {
-        public string Title { get; init; } = title;
-        public string Code { get; init; } = code;
-        public Dictionary<string, Seminar> Seminars { get; } = new();
-        public Seminar? Lecture { get; set; }
+        private Dictionary<string, Seminar> _seminars = new();
+        private ReadOnlyDictionary<string, Seminar> _seminarsView;
+        private Seminar? _lecture;
+
+        public string Title { get; }
+        public string Code { get; }
+
+        public IReadOnlyDictionary<string, Seminar> Seminars => _seminarsView;
+        public Seminar? Lecture 
+        { 
+            get
+            {
+                return _lecture;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    if (value.Id != Code)
+                    {
+                        throw new ArgumentException("Lecture code and Course code must match.");
+                    }
+                    if (!value.IsLecture)
+                    {
+                        throw new ArgumentException("Lecture must have IsLecture set to true.");
+                    }
+                }
+                _lecture = value;
+            }
+        }
+
+        public Course(string title, string code)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(title);
+            ArgumentException.ThrowIfNullOrWhiteSpace(code);
+
+            Title = title;
+            Code = code;
+
+            _seminarsView = _seminars.AsReadOnly();
+        }
+
+        public void AddSeminar(Seminar seminar)
+        {
+            ArgumentNullException.ThrowIfNull(seminar);
+
+            if (_seminars.ContainsKey(seminar.Id))
+            {
+                throw new ArgumentException($"Course already has a seminar with id {seminar.Id}.", nameof(seminar));
+            }
+            if (!seminar.Id.StartsWith(Code + "/"))
+            {
+                throw new ArgumentException("Seminar id must start with <course code>/");
+            }
+            if (seminar.IsLecture)
+            {
+                throw new ArgumentException("Seminars must have IsLecture set to false.");
+            }
+
+            _seminars.Add(seminar.Id, seminar);
+        }
+
+        public void RemoveSeminar(string id)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(id);
+
+            if (!_seminars.ContainsKey(id))
+            {
+                throw new ArgumentException($"Seminar with id {id} does not exist.", nameof(id));
+            }
+
+            _seminars.Remove(id);
+        }
     }
 }
