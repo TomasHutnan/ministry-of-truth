5a0763ef6dbc464128c68793794390f3809a519d
TomasHutnan <tomas.hutnan147@gmail.com>
2026-02-22 12:41:48 +0100

    chore: refactored Seminar for better encapsulation

--- HW00/timetable/seminar.cs
+++ HW00/timetable/seminar.cs
@@ -1,20 +1,53 @@
+using System.Collections.Immutable;
+using System.Collections.ObjectModel;
+
 namespace pb178.timetable
 {
-    public class Seminar(string id, bool isLecture, string[] teacherNames)
+    public class Seminar
     {
-        public string Id { get; init; } = id;
-        public bool IsLecture { get; init; } = isLecture;
-        public string[] TeacherNames { get; init; } = teacherNames;
-        public List<Session> Sessions = [];
+        private readonly List<Session> _sessions = [];
+        private readonly ReadOnlyCollection<Session> _sessionsView;
+
+        public string Id { get; }
+        public bool IsLecture { get; }
+
+        public ImmutableArray<string> TeacherNames { get; }
+        public IReadOnlyList<Session> Sessions => _sessionsView;
+
+        public Seminar(string id, bool isLecture, string[] teacherNames)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(id);
+            ArgumentNullException.ThrowIfNull(teacherNames);
+
+            Id = id;
+            IsLecture = isLecture;
+            TeacherNames = [.. teacherNames];
+
+            _sessionsView = _sessions.AsReadOnly();
+        }
 
         public void AddWeeklySession(DateTime firstSessionStart, DateTime firstSessionEnd, string? classroomId, int repetitionCount)
         {
+            AddWeeklySession(new Session(firstSessionStart, firstSessionEnd, classroomId), repetitionCount);
+        }
+
+        public void AddWeeklySession(Session firstSession, int repetitionCount)
+        {
+            ArgumentOutOfRangeException.ThrowIfLessThan(repetitionCount, 1, nameof(repetitionCount));
+
             TimeSpan week = new(7, 0, 0, 0);
 
-            for (int i = 0; i < repetitionCount; i++)
+            _sessions.Add(firstSession);
+
+            for (int i = 1; i < repetitionCount; i++)
             {
-                Sessions.Add(new Session(firstSessionStart + week * i, firstSessionEnd + week * i, classroomId));
+                _sessions.Add(new Session(firstSession.Start + week * i, firstSession.End + week * i, firstSession.ClassroomId));
             }
+        }
+
+        public void AddSession(Session session)
+        {
+            _sessions.Add(session);
         }
     }
 }
