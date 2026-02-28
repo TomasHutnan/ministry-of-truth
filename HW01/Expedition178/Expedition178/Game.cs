8d93a61b695d0b3fa9a7085753ff2608f9ab6f55
TomasHutnan <tomas.hutnan147@gmail.com>
2026-02-28 16:23:15 +0100

    feat: game loop

--- HW01/Expedition178/Expedition178/Game.cs
+++ HW01/Expedition178/Expedition178/Game.cs
@@ -2,14 +2,112 @@
 using System.Collections.Generic;
 using System.Text;
 using Expedition178.Interface;
+using Expedition178.Team;
+using Expedition178.Utils;
+using Expedition178.Enumerate;
 
 namespace Expedition178
 {
     public class Game : IGame
     {
+        private Player? _player;
+        private Enemy? _enemy;
+        private static readonly Battle _battler = new Battle();
+
+        private int wavesBeaten = 0;
+
         public void Start()
         {
+            Console.WriteLine("Welcome to Expedition 178!");
 
+            _player = new Player();
+            _enemy = new Enemy();
+
+            Console.WriteLine("Your commands are: check, fight, info, sort, quit.");
+
+            Run();
+        }
+
+        private void Run()
+        {
+            bool isRunning = true;
+            while (isRunning)
+            {
+                switch (Input.GetCommand())
+                {
+                    case CommandType.Check:
+                        Console.WriteLine("You see the next wave of monsters:");
+                        _enemy!.WriteStats();
+                        break;
+                    case CommandType.Fight:
+                        Fight();
+                        if (wavesBeaten >= 3)
+                        {
+                            isRunning = false;
+                        }
+                        break;
+                    case CommandType.Info:
+                        Console.WriteLine($"Waves defeated: {wavesBeaten}, the adventurers:");
+                        _player!.WriteStats();
+                        break;
+                    case CommandType.Sort:
+                        _player!.SortAdventurers();
+                        break;
+                    case CommandType.Quit:
+                        isRunning = false;
+                        break;
+                }
+            }
+
+            if (wavesBeaten >= 3)
+            {
+                Console.WriteLine($"Congratulations! You have completed Expedition 178 by defeating {wavesBeaten} waves of monsters!");
+            }
+            else
+            {
+                Console.WriteLine("You have lost the game :(");
+            }
+
+            Console.WriteLine("Thanks for playing Expedition 178!");
+        }
+
+        private void Fight()
+        {
+            Team.Team winner = _battler.Fight(_player!, _enemy!);
+
+            if (winner == _player)
+            {
+                wavesBeaten++;
+                int experienceGained = Random.Shared.Next(100, 200);
+                int levelUpCount = _player.GainExperience(experienceGained);
+
+                Console.Write($"You won the battle! ");
+                WriteExperienceGained(experienceGained, levelUpCount);
+                Console.WriteLine();
+
+                _enemy = new Enemy();
+                _player.HealTeam();
+                _player.ResetOrder();
+            }
+            else
+            {
+                wavesBeaten++;
+                int experienceGained = Random.Shared.Next(100, 200);
+                int levelUpCount = _player!.GainExperience(experienceGained);
+
+                Console.Write($"The adventurers lost the battle. ");
+                WriteExperienceGained(experienceGained, levelUpCount);
+                Console.WriteLine();
+            }
+        }
+
+        private void WriteExperienceGained(int experienceGained, int levelUpCount)
+        {
+            Console.Write($"The adventurers gained {experienceGained} XP!");
+            if (levelUpCount > 0)
+            {
+                Console.Write($" The adventurers leveled up {levelUpCount} time{(levelUpCount > 1 ? "s" : "")}!");
+            }
         }
     }
 }
