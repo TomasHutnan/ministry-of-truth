8edff3323dc24233a0c3a75a37d3e5022d28e113
TomasHutnan <tomas.hutnan147@gmail.com>
2026-03-01 01:54:22 +0100

    feat: battler test suite

---
+++ HW01/Expedition178/Expedition178.Test/TestHelpers.cs
@@ -0,0 +1,24 @@
+﻿using Expedition178.Entity;
+using Expedition178.Enumerate;
+
+namespace Expedition178.Test
+{
+    public static class TestHelpers
+    {
+        public static Adventurer CreateAdventurer(
+            string name = "TestAdventurer",
+            int attack = 10,
+            int health = 10,
+            int speed = 10,
+            AttackType type = AttackType.Physical)
+            => new(name, attack, health, speed, type);
+
+        public static Monster CreateMonster(
+            string name = "TestMonster",
+            int attack = 10,
+            int health = 10,
+            int speed = 10,
+            MonsterType type = MonsterType.Nature)
+            => new(name, attack, health, speed, type);
+    }
+}
