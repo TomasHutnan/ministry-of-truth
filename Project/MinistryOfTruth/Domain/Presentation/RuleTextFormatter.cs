df10605c6bfc02e82c68a58b66e0df1431808b1b
TomasHutnan <tomas.hutnan147@gmail.com>
2026-04-21 16:42:54 +0200

    feat: rule text formatter stub

--- Project/MinistryOfTruth/Domain/Presentation/RuleTextFormatter.cs
+++ Project/MinistryOfTruth/Domain/Presentation/RuleTextFormatter.cs
@@ -1,5 +1,12 @@
-﻿namespace MinistryOfTruth.Domain.Presentation;
+﻿using MinistryOfTruth.Domain.Models;
+
+namespace MinistryOfTruth.Domain.Presentation;
 
 public class RuleTextFormatter
 {
+    public string BuildRuleText(Rule rule)
+    {
+        // TODO: Add more variants - maybe introduce levels of agrresivity
+        return $"Any mention of {rule.Keyword} is prohibited.";
+    }
 }
